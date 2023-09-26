b43e26da249830826ca2bebd714970c0e938b441
Pascal Garcia Esteves <58555606+PascalEsteves@users.noreply.github.com>
2023-09-27 00:37:53 +0100

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -15,21 +15,21 @@ def get_chunks_from_excel(doc):
     df = pd.read_excel(doc)
     text=""
     for _, row in df.iterrows:
-        text+=str(row)
+        text+=str(row)+"\n"
     return text
 
 def get_chunks_from_csv(doc):
     df = pd.read_csv(doc)
     text=""
     for _, row in df.iterrows:
-        text+=str(row)
+        text+=str(row)+"\n"
     return text
 
 def get_chunks_from_pdf(doc):
     pdf_reader = PdfReader(doc)
     text = ""
     for page in pdf_reader.pages:
-        text +=page.extract_text()
+        text +=page.extract_text()+"\n"
     return text
 
 def get_chunks_from_doc(docs):
@@ -37,11 +37,12 @@ def get_chunks_from_doc(docs):
     text = ""
     for doc in docs:
         if check_excel(doc):
-            text+=get_chunks_from_excel(doc)
+            text+=get_chunks_from_excel(doc)+"\n"
         elif check_csv(doc):
-            text+=get_chunks_from_csv(doc)
+            text+=get_chunks_from_csv(doc)+"\n"
         elif check_pdf(doc):
-            text+=get_chunks_from_csv(doc)
+            text+=get_chunks_from_csv(doc)+"\n"
         else:
-            print('The ingetested format is not accepatable')
+            print(doc, "-" , 'The ingetested format is not accepatable')
 
+        return text
